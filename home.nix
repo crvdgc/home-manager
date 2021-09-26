62904410f8227e8c0ee333c8ec7fdb7e72e7515b
ubikium <ubikium@gmail.com>
2021-09-26 10:21:23 +0900

    initial home manager files

---
+++ home.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "ubikium";
+  home.homeDirectory = "/home/ubikium";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.05";
+
+  home.packages = with pkgs; [
+    neovim
+    unzip
+    agda
+    emacs
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "ubikium";
+    userEmail = "ubikium@gmail.com";
+  };
+}
